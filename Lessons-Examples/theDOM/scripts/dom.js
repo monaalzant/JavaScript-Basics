9c926849bd87b5cbc7a421b9e3035aad8fca94a6
pixel2code <luke@pixeltocode.co>
2020-04-01 11:45:13 +0100

    Moved examples in to respected folfers and created a DOM project for the up and coming lesson

---
+++ Lessons-Examples/theDOM/scripts/dom.js
@@ -0,0 +1,27 @@
+// By ID
+let hobbielist = document.getElementById('hobby-list');
+
+// by Tag Name
+let hobbies = document.getElementsByTagName('li');
+
+// By Class Name
+let alsoHobbies = document.getElementsByClassName('hobby');
+
+// By CSS Query
+let firstHobby = document.querySelector('ul li.hobby');
+
+let againAlsoHobbies = document.querySelectorAll('ul li.hobby');
+
+
+
+// Returning element vs Array of Elements
+
+// let firstHobbyItem = alsoHobbies[0];
+
+// console.log(firstHobbyItem);
+
+
+// let uxDesignImage = document.getElementById('ux-design');
+// let oldImageSource = uxDesignImage.src;
+
+//     uxDesignImage.src = 'https://images.unsplash.com/photo-1558403194-611308249627?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80';
