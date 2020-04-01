35977c1194c4e63d75c2868c16734af23f0abfb2
pixel2code <luke@pixeltocode.co>
2020-04-01 11:50:34 +0100

    DATA TYPE EXAMPLES ADDED

---
+++ Lessons-Examples/dataTypes/dataTypes.js
@@ -0,0 +1,43 @@
+// Primitive Data Types ( Examples )
+
+// String
+const name = 'Luke Johnson';
+console.log(typeof name);
+
+// Number
+const age = 35;
+console.log(typeof age);
+
+// Boolean
+const hasKids = true;
+console.log(typeof hasKids);
+
+// Null
+const car = null;
+console.log(typeof car);
+
+// Undefined
+let test;
+console.log(test);
+
+// Symbol - ES6
+const sym = Symbol();
+console.log(typeof sym);
+
+
+// Reference Types - Objects ( Examples )
+
+// Array
+const hobbies = ['movies', 'music'];
+console.log(typeof hobbies);
+
+// Object literal
+const address = {
+  city: 'Greenhithe & Swanscombe',
+  county: 'Kent'
+}
+console.log(typeof address);
+
+// Date
+const today = new Date();
+console.log(typeof today);
